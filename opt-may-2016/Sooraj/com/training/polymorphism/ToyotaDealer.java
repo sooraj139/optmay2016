9733d9b1ab84b9bdce7a6b5d636703fb27d85aaf
Sooraj Kesavadas <sooraj139@gmail.com>
2016-06-28 21:58:17 -0400

    Update ToyotaDealer.java

--- opt-may-2016/Sooraj/com/training/polymorphism/ToyotaDealer.java
+++ opt-may-2016/Sooraj/com/training/polymorphism/ToyotaDealer.java
@@ -2,9 +2,9 @@ package com.training.polymorphism;
 
 public class ToyotaDealer {
 	public void implementation(vehicle T){
-		T.name();
-	    T.cost();
-		T.make();
+		T.goFast();
+	        T.stop();
+		T.reverse();
 		
 	}
 
