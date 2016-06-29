4b5162c6ad7cf87004567c1517531716157a0733
Sooraj Kesavadas <sooraj139@gmail.com>
2016-06-28 20:41:58 -0400

    Update DMV.java

--- opt-may-2016/Sooraj/com/training/polymorphism/DMV.java
+++ opt-may-2016/Sooraj/com/training/polymorphism/DMV.java
@@ -2,9 +2,9 @@ package com.training.polymorphism;
 
 public class DMV extends CarWash {
 	public void Truck1(vehicle v){
-		v.name();
-		v.cost();
-		v.make();
+		v.goFast();
+		v.stop();
+		v.reverse();
 		
 	}
 
